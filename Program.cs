2bdd5e2d9d09c9ba479808ad8edec5d7d73f8787
Yosmel Chiang <yosmelchiang@gmail.com>
2026-02-16 10:43:38 +0100

    add simple code to debug

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Debugging
+{
+    public class Program
+    {
+        public static void CountDown(int n)
+        {
+            string? message = null;
+
+            int i = n;
+            while (i > 0)
+            {
+                message = $"Countdown: {i}";
+                Console.WriteLine(message);
+                i--;
+            }
+
+            if (message != null)
+            {
+                Console.WriteLine("Countdown finished");
+            }
+        }
+        public static void Main()
+        {
+            CountDown(5);
+        }
+    }
+}
